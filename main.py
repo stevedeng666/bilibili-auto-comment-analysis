dd729a166cd9eeb2d8b5957689532bb2b2a29977
steve <steveharris@qq.com>
2025-05-16 15:22:32 +0800

    further perfected

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from scrape import *
 import process
+from fastapi import Body
+import asyncio
+from pydantic import BaseModel
+from typing import List, Any
 app = FastAPI()
-
+class plotData(BaseModel):
+    keyword:str
+    data:List[Any]
 
 @app.get("/")
 def read_root():
@@ -18,3 +25,10 @@ def get():
 @app.get("/search")
 def search_items(keyword:str):
     return process.parse(keyword)
+
+@app.post("/plot")
+def plot_chart(requestData:plotData):
+    data=requestData.dict()
+    fig = process.sentimentPlot(*process.comment2list(data))
+    html = fig.to_html(full_html=True, include_plotlyjs=True)
+    return HTMLResponse(content=html)
--- process.py
+++ process.py
@@ -1,5 +1,8 @@
 import scrape
-import asyncio
+#import asyncio
+import plotly.express as px
+from snownlp import SnowNLP
+import pandas as pd
 '''
 #异步版，容易封IP，我已经被封了好几次了555
 def parse(keyword):
@@ -41,21 +44,32 @@ def parse(keyword):
     results=scrape.bilibiliSearch(keyword)
     commentSet={"keyword":keyword,"data":[]}
     for result in results:
-        try:
-            comments=scrape.bilibiliComment(result['id'])
-            commentSet['data'].append(
-                {
-                    "id":result["id"],
-                    'title':result['title'],
-                    'comments':[{"时间戳":comment['ctime'], 
-                                '内容':comment['content']['message'],
-                                '点赞数':comment['like'], 
-                                '回复':[{"时间戳":reply['ctime'], 
-                                        '内容':reply['content']['message'],
-                                        '点赞数':reply['like']} for reply in comment['replies']]} for comment in comments]
-                }
-            )
-        except:
-            pass
+        comments=scrape.bilibiliComment(result['id'])
+        if comments is None:
+            continue
+        commentSet['data'].append(
+            {
+                "id":result["id"],
+                'title':result['title'],
+                'comments':[{"时间戳":comment['ctime'], 
+                            '内容':comment['content']['message'],
+                            '点赞数':comment['like'], 
+                            '回复':[{"时间戳":reply['ctime'], 
+                                    '内容':reply['content']['message'],
+                                    '点赞数':reply['like']} for reply in comment['replies']]} for comment in comments]
+            }
+        )
     return commentSet
 
+def comment2list(data:dict):
+    commentList=[]
+    for i in data['data']:
+        for j in i['comments']:
+            text=j['内容'].replace("\n","")
+            commentList.append(j['内容'])
+    return commentList,data['keyword']
+def sentimentPlot(commentList,title):
+    attrs=['Positive' if SnowNLP(comment).sentiments>0.5 else 'Negative' for comment in commentList]
+    data=pd.Series(attrs).value_counts().reset_index(name="count")
+    fig=px.pie((data),names="index",values='count',title=f'关键词:{title} 情感分析',color="index",color_discrete_map={"Negative":"red","Positive":"green"})
+    return fig
